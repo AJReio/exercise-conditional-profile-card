04dc142e5012b9f021d83197fe0a4f4bc3676b0c
AJReio <alejandrorello@gmail.com>
2024-10-09 19:18:46 +0000

    Primer commit

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,30 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name ? variables.name : "Alex "}${
+    variables.lastName ? variables.lastName : "Rello"
+  } </h1>
+          <h2>${variables.role ? variables.role : "Dj / Event Promoter"}</h2>
+          <h3>${variables.city ? variables.city : "Madrid /"}${
+    variables.country ? variables.country : "Spain"
+  }</h3>
+          <ul class="${
+            variables.socialMediaPosition === "position-right"
+              ? "position-right"
+              : "position-left"
+          }">
+            <li><a href="https://twitter.com/${
+              variables.twitter ? variables.twitter : "@Lord_Voldemort7"
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github ? variables.github : "AJReio"
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${
+              variables.linkedin ? variables.linkedin : "alesanchezr"
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram ? variables.intsgram : "reiohuray"
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
